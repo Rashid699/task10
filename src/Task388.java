c02e3d5455b7a09466ec32f70f3ee14d5c480a41
Rashid <alshiblir650@gmail.com>
2026-09-15 19:23:27 +0400

    task388

---
+++ src/Task388.java
@@ -0,0 +1,25 @@
+public class Task388 {
+
+    public static void printReverse(String text) {
+
+
+        if (text.isEmpty()) {
+            return;
+        }
+
+
+        printReverse(text.substring(1));
+
+
+        System.out.println(text.charAt(0));
+    }
+
+    public static void main(String[] args) {
+
+        printReverse("cat");
+
+        System.out.println();
+
+        printReverse("hello");
+    }
+}
